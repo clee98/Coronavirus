e809b931e13017827875a1c2c8aa6ac0ced55805
Minjae <minjae.jung0@gmail.com>
2020-03-18 18:01:47 -0400

    somedata

--- javascript.js
+++ javascript.js
@@ -1,7 +1,24 @@
 $.ajax({
-    url: 'https://thevirustracker.com/free-api?countryTimeline= ',
+    url: 'https://thevirustracker.com/free-api?global=stats',
     dataType: 'json',
     success: function(data) {
-      console.log(data);
+    //   console.log(data);
+
+      var x = data
+      var info = x.results[0]
+      console.log(x)
+      console.log(info)
+
+      const okeys = Object.keys(info)
+
+      for(const key of okeys){
+          console.log(key)
+          console.log(info[key])
+          $(".totals").append(`<li>${key}</li>`)
+      }
+
+   
+
+
     }
   });
