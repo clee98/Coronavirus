a28e64115743ea29e64f5d5fc0eb56f70855c4f4
Minjae <minjae.jung0@gmail.com>
2020-03-19 16:29:24 -0400

    jsupdate

--- javascript.js
+++ javascript.js
@@ -2,18 +2,18 @@ $.ajax({
     url: 'https://thevirustracker.com/free-api?global=stats',
     dataType: 'json',
     success: function(data) {
-    //   console.log(data);
+      console.log(data);
 
       var x = data
       var info = x.results[0]
-      console.log(x)
-      console.log(info)
+    //   console.log(x)
+    //   console.log(info)
 
       const okeys = Object.keys(info)
 
       for(const key of okeys){
-          console.log(key)
-          console.log(info[key])
+        //   console.log(key)
+        //   console.log(info[key])
           if(key != "source"){
             $(".totals").append(`<li>${key} : ${info[key]}</li>`)
           }
@@ -24,3 +24,22 @@ $.ajax({
 
     }
   });
+
+  
+
+$.ajax({
+  url: 'https://thevirustracker.com/free-api?countryTimeline=US',
+  dataType: 'json',
+  success: function(data) {
+    console.log(data);
+  }
+});
+
+
+$.ajax({
+  url: 'https://thevirustracker.com/free-api?countryTotal=KR',
+  dataType: 'json',
+  success: function(data) {
+    console.log(data);
+  }
+});
